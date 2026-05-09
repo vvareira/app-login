8207d75105313c599042b2e133bc850ea4fc4426
unknown <you@example.com>
2026-05-08 22:05:55 -0300

    test

---
+++ test/loginTest.js
@@ -0,0 +1,15 @@
+import { realizarLogin } from "../src/Login.js";
+import assert from 'node:assert';
+
+describe('Validar login', () => {
+    it('Login válido', () => {
+        const login = 'vanessa';
+        const senha = '123456';
+
+      const resultadoEsperado = realizarLogin(login, senha);
+
+
+      assert.equal(resultadoEsperado, 'Logado com sucesso.')
+        
+    });
+});
